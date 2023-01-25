3d496edcab153ce610d36e105d9b1c3534ae25de
dbpython <33459750+papismurf@users.noreply.github.com>
2023-01-25 13:25:10 +0000

    Update index.py
    Update endpoints for serialization preparation

--- api/index.py
+++ api/index.py
@@ -7,23 +7,37 @@ from api.model.transaction_type import TransactionType
 app = Flask(__name__)
 
 
+transactions = [
+    Income('Remittance', 5000),
+    Income('Dividends', 200),
+    Income('Dinner', 50),
+    Expense('Travel', 100)
+]
+
 @app.route('/')
 def home():  # put application's code here
     return 'Home'
 
 
-incomes = [
-    {'description': 'salary', 'amount': 120000}
-]
-
 app.route('/incomes')
 def get_incomes():
+    schema = IncomeSchema(many=True)
+    incomes = schema.dump(
+        filter(lambda t: t.type == TransactionType.INCOME, transactions)
+    )
     return jsonify(incomes)
 
 
 app.route('/incomes', methods=['POST'])
 def add_income() -> tuple[str, int]:
-    incomes.append(request.get_json())
+    income = IncomeSchema.load(request.get_json())
+    transactions.append(income)
+    return '', 204
+
+app.route('/expenses')
+def add_expense():
+    expense = ExpenseSchema().load(request.get_json())
+    transactions.append(expense)
     return '', 204
 
 
